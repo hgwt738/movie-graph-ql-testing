18bb721dc8b1a4c2ab01cf1e9ca6bbea3c61e091
jacob <hgwt738@gmail.com>
2021-10-13 21:07:03 +0900

    Updated ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/client'
 
 import App from './components/App'
+import client from './apollo'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
   document.getElementById('root')
 )
